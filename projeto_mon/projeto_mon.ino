1b6400437ff42b16b5251d45bed25e43dafa79c1
Gabriel <gabrielbotogoske@gmail.com>
2022-11-14 15:08:26 -0600

    ADC

--- projeto_mon/projeto_mon.ino
+++ projeto_mon/projeto_mon.ino
@@ -1,9 +1,49 @@
-void setup() {
-  // put your setup code here, to run once:
+//GABRIEL BOTOGOSKE
+//SBND-FERMILAB
+//PROJECT LIGHT MONITORING
 
+#define  SiPM_n 4
+
+//set the readout pins as ADC
+const int SiPM[SiPM_n]={A0,A1,A2,A3};
+int SiPM_ADC[SiPM_n]={};
+double SiPM_mV[SiPM_n]={};
+
+//precision of ADC in bits
+const int precision = 12; 
+
+//precision of ADC in voltage (mV)
+const double resolution=3300/(pow(2,precision)-1);
+
+void setup() 
+{
+
+  //set the resolution of ADC as 12 bits
+  //this means a resolution of 3300mV/(2¹²-1) = 805.96 uV
+  analogReadResolution(12);
+
+  //For debug in serial monitor
+  Serial.begin(9600);
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  //read SIPM in adc_channels
 
+  for(int i=0;i<SiPM_n;i++)
+  {
+    Serial.print("Channel ");
+    Serial.print(i);
+    Serial.print(": ");
+    SiPM_ADC[i]=analogRead(SiPM[i]);
+    SiPM_mV[i]=SiPM_ADC[i]*resolution;
+    Serial.print(SiPM_mV[i]);
+    Serial.print(" mV \n");
+  }
+  
+  
+  Serial.print("\n");   
+  delay(1000);
+  //Serial.print(resolution);
 }
